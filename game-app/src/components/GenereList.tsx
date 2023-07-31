e62bb4489a107e534490c04450ae9b50a40e2b70
Talha Tariq <tariqtalha936@gmail.com>
2023-07-31 18:13:41 +0500

    Show Spinner

--- game-app/src/components/GenereList.tsx
+++ game-app/src/components/GenereList.tsx
@@ -1,13 +1,26 @@
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGeneres, { Genre } from "../hooks/useGeneres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenereList = () => {
-  const { data } = useGeneres();
+  const { data, isLoading, error } = useGeneres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
-    <ul>
+    <List>
       {data.map((gener) => (
-        <li key={gener.id}>{gener.name}</li>
+        <ListItem key={gener.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(gener.image_background)}
+            />
+            <Text fontSize="lg">{gener.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
